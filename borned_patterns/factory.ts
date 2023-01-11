13f81d5cd97d85724732dcac0e4d674e6c526d42
svyat174 <svyatgluhov@gmail.com>
2023-01-11 18:37:17 +0300

    factory pattern 11.01.23 18:35

---
+++ borned_patterns/factory.ts
@@ -0,0 +1,43 @@
+interface IFactoryInsurance {
+  operation(): string;
+}
+
+class ABInsurance implements IFactoryInsurance {
+  operation(): string {
+    return "AB insurance";
+  }
+}
+
+class CDInsurance implements IFactoryInsurance {
+  operation(): string {
+    return "CD insurance";
+  }
+}
+
+abstract class CreateInsurance {
+  public abstract fabricMethod(): IFactoryInsurance;
+
+  public someOperations() {
+    const ins = this.fabricMethod();
+    return `Was create ${ins.operation()}`;
+  }
+}
+
+class ABInsuranceCreate extends CreateInsurance {
+  public fabricMethod(): IFactoryInsurance {
+    return new ABInsurance();
+  }
+}
+
+class CDInsuranceCreate extends CreateInsurance {
+  public fabricMethod(): IFactoryInsurance {
+    return new CDInsurance();
+  }
+}
+
+function client(cretor: CreateInsurance) {
+  console.log(cretor.someOperations());
+}
+
+client(new ABInsuranceCreate());
+client(new CDInsuranceCreate());
